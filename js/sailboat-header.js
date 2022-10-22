5c95e3602d09483c1e637b8bf783bfe0a1bad768
Yannis Gkoumas <ygkoumas@gmail.com>
2022-10-23 00:35:11 +0300

    add header graphics

---
+++ js/sailboat-header.js
@@ -0,0 +1,92 @@
+
+
+function createHalfWave(color, waveLength,waveAmplitude){
+    const marginBottom = parseInt((50 - waveAmplitude)/2)+8;
+    return `<div style="
+        display: inline-block; 
+        border: 2px solid ${color};
+        background: ${color}; 
+        border-radius: 50px;
+        width: ${waveLength}px;
+        height: ${waveAmplitude}px;
+        margin-bottom: ${marginBottom}px">
+    </div>`
+}
+function createWaveWrapper(halfWaves){
+    return `<div style="
+        position: relative;
+        white-space: nowrap;
+        width: 100%;
+        overflow: hidden;
+        background-image: linear-gradient( to bottom, #fff, #fff 50%, #0652e1 15% );
+        border-bottom: 25px solid #0652e1;
+        padding: 5px 0">
+        ${halfWaves}
+    </div>`
+}
+
+let wave = '';
+for (let _ = 0; _<=200; _++) {
+    waveLength = 25 + 50 * Math.random();
+    wave += createHalfWave(_%2 ? '#0652e1' : '#fff', waveLength, waveLength);
+}
+
+function createBoat(position) {
+
+    return `<div style="
+        width: 70px;
+        position: absolute;
+        bottom: 5px;
+        left: ${position}px;
+        border-top: 13px solid #543;
+        border-right: 20px solid transparent;
+        border-left: 8px solid transparent;  
+        ">
+            <div style="
+                width: 3px;
+                height: 75px;
+                background: #543;
+                position: absolute;
+                bottom: 0;
+                left: 50px;">
+            </div>
+            <div style="
+                width: 2px;
+                height: 43px;
+                background: #543;
+                position: absolute;
+                bottom: 50px;
+                left: 32px;
+                transform: rotate(117deg);">
+            </div>
+            <div style="
+                height: 48px;
+                position: absolute;
+                bottom: 14px;
+                left: 15px;
+                border-left: 34px solid #eccaa6;
+                border-top: 17px solid transparent;">
+            </div>
+    </div>`;
+}
+console.info($(window).width());
+
+const windowWidth = $(window).width();
+function setBoatPosition() {
+    return Math.random()*(windowWidth - 100);
+}
+let firstBoatPosition = setBoatPosition();
+let secondBoatPosition = setBoatPosition();
+if (windowWidth < 300) {
+    secondBoatPosition = 400;
+}
+while (Math.abs(firstBoatPosition - secondBoatPosition) < 70) {
+    firstBoatPosition = setBoatPosition();
+    secondBoatPosition = setBoatPosition();
+}
+const boats = createBoat(firstBoatPosition) + createBoat(secondBoatPosition);
+$( document ).ready(function() {
+    $( createWaveWrapper(wave+boats) ).insertAfter( ".site-header" );
+});
+
+
